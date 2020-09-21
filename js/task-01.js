2dd321c612137baf2b3818c05a65a70cc53d2d8a
Malyshko Serhii <malyshkosergiy@gmail.com>
2020-09-21 14:58:31 +0300

    btn start is added

---
+++ js/task-01.js
@@ -0,0 +1,28 @@
+const bodyRef = document.querySelector("body");
+пше;
+
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const changeColor = {
+  intervalId: null,
+
+  start() {
+    this.intervalId = setInterval(() => {
+      bodyRef.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length)];
+    }, 1000);
+  },
+};
+
+bodyRef.addEventListener("click", changeColor.start.bind(changeColor));
